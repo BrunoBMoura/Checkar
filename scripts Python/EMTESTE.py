8dff0e82b70cb5a2f4ede61b68f29d4f531e0618
Victor Lúcio <victor.lucio10@gmail.com>
2017-09-01 00:12:33 -0300

    Add files via upload
    Agora funciona, mas tem uns probleminhas:
    --Ambiguidade de carros

--- scripts Python/EMTESTE.py
+++ scripts Python/EMTESTE.py
@@ -13,7 +13,7 @@ def get_task(placa):
 	result = sc.search(placa)
 
 	response = urllib.request.urlopen("http://fipeapi.appspot.com/api/1/carros/marcas.json")
-	data = json.loads(response.read().decode)
+	data = json.loads(response.read().decode())
 	print(result)
 	marcaCarro = result['brand']
 
@@ -24,7 +24,9 @@ def get_task(placa):
 	brandList = {}
 	for instance in data:
 		brandList[instance['name']] = instance['id']
-	closeBrand = difflib.get_close_matches(marcaCarro, brandList.keys())
+
+	print(brandList)
+	closeBrand = difflib.get_close_matches(marcaCarro, brandList.keys(), 1, 0.1)
 	brand = closeBrand[0]
 	idMarca = brandList[brand]
 
@@ -36,19 +38,23 @@ def get_task(placa):
 	for instance2 in data: #criando lista de modelos daquela marca
 		listaModelos[instance2['name']] = instance2['id']
 
+	print(listaModelos)
 	modeloCarro = result['brand'].upper()
 	
 	if brand in modeloCarro:
 		modeloCarro = modeloCarro.replace(brand, '')
+	elif 'VW' in modeloCarro:
+		modeloCarro = modeloCarro.replace('VW', '')
 	
-	print(modeloCarro)
-	closeMatches = difflib.get_close_matches(modeloCarro,listaModelos.keys())
+	# print(modeloCarro)
+	closeMatches = difflib.get_close_matches(modeloCarro,listaModelos.keys(), 1, 0.1)
+	print(closeMatches)
 
-	indexListaIdModeloCarro = listaModelos.index(closeMatches[0])
+	IDcarro = listaModelos[closeMatches[0]]
 
 	#---------------Pegando Modelo-Ano
-	response = urllib.request.urlopen("http://fipeapi.appspot.com/api/1/carros/veiculo/"+str(idMarca)+"/"+str(listaIdModelos[indexListaIdModeloCarro])+".json") #busca todos os modelos daquela marca
-	data = simplejson.load(response)
+	response = urllib.request.urlopen("http://fipeapi.appspot.com/api/1/carros/veiculo/"+str(idMarca)+"/"+str(IDcarro)+".json") #busca todos os modelos daquela marca
+	data = json.loads(response.read().decode())
 	anoPlaca = result['model_year']
 	for percorredor in data:
 		anoFipe = percorredor["fipe_codigo"][0:(percorredor["fipe_codigo"].find("-"))]
@@ -56,8 +62,8 @@ def get_task(placa):
 			break
 
 	#--------------Pegand valor do carro
-	response = urllib.request.urlopen("http://fipeapi.appspot.com/api/1/carros/veiculo/"+str(idMarca)+"/"+str(listaIdModelos[indexListaIdModeloCarro])+"/"+percorredor["fipe_codigo"]+".json") #busca todos os modelos daquela marca
-	data = simplejson.load(response)
+	response = urllib.request.urlopen("http://fipeapi.appspot.com/api/1/carros/veiculo/"+str(idMarca)+"/"+str(IDcarro)+"/"+percorredor["fipe_codigo"]+".json") #busca todos os modelos daquela marca
+	data = json.loads(response.read().decode())
 	result['preco'] = data['preco']
 	return jsonify(result)
 
