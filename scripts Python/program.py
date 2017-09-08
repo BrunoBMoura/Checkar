24a0b5e4fd7979bfabac47a914b7554fd19d1720
Ricardo <31243387+RicardoEN@users.noreply.github.com>
2017-09-07 23:31:19 -0300

    Add files via upload

---
+++ scripts Python/program.py
@@ -0,0 +1,57 @@
+import requests
+import urllib.request
+import re
+import difflib
+from os import *
+from bs4 import BeautifulSoup
+from unidecode import *
+
+#funcao para pegar informacoes de um carro no site carrosnaweb.com.br
+def info_cnw(marca, anoModelo):
+	separador = marca.find("/")
+	fabricante = marca[0:separador]
+	modelo = marca[separador+1:marca.find(" ")]
+	ano = anoModelo
+	carro = marca.replace("/", " ")
+
+	#url = "http://www.carrosnaweb.com.br/avancada.asp"
+	url = "http://www.carrosnaweb.com.br/catalogo.asp?anoini=" + ano + "&anofim=" + ano + "&valfim=100000000&config=0&combustivel=0&cambio=0&proced=0&fabricante=" + fabricante + "&porte=0&varnome=" + modelo + "&portas=&ordem=1&passageiros=0"
+	#url = "http://www.carrosnaweb.com.br/catalogo.asp?anoini=1990&anofim=1990&valfim=100000000&config=0&combustivel=0&cambio=0&proced=0&fabricante=FORD&porte=0&varnome=ESCORT&portas=&ordem=1&passageiros=0"
+
+	try:
+		html = requests.get(url)
+		text = html.text
+		soup = BeautifulSoup(text, "html5lib")
+		#x = soup.select("table tr td table tr td a")
+		x = soup.findAll(href=re.compile("fichadetalhe"))
+		#x = soup.findAll('a', href=True)
+		y = x[1::2]
+		title = []
+		href = []
+		for a in y:
+			title.append(a['title'])
+			href.append(a['href'])
+		closeMatch = difflib.get_close_matches(carro,title, 1, 0.1)[0]
+		endereco = "http://www.carrosnaweb.com.br/" + href[title.index(closeMatch)]
+		print("Modelo: " + closeMatch)
+		print("Link: " + endereco)
+
+		try:
+			html = requests.get(endereco)
+			text = html.text
+			soup = BeautifulSoup(text, "html5lib")
+			x = soup.findAll(href=re.compile(modelo.lower()))
+			for a in x:
+				print("http://www.carrosnaweb.com.br/" + a['href'])
+
+		except urllib.error.HTTPError:
+			pass
+
+	except urllib.error.HTTPError:
+		pass
+
+#marca = "FORD/ESCORT 1.8 XR3"
+#anoModelo = "1990"
+marca = "PEUGEOT/HOGGAR ESCAPADE"
+anoModelo = "2012"
+info_cnw(marca, anoModelo)
