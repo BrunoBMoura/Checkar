9f8b70edecd87b8ded7b6a57773467a43c36271a
BuddhaIsTheAnswer <brunomoura.meta@gmail.com>
2017-09-08 12:59:02 -0300

    Link alterado

--- src/providers/carInfo/carInfo.ts
+++ src/providers/carInfo/carInfo.ts
@@ -8,7 +8,7 @@ export class CarInfoProvider {
 
   constructor(public http: Http) {
     console.log('Hello CarInfoProvider Provider');
-    this.url = 'https://checkcarapp.herokuapp.com';
+    this.url = 'http://flask-env.f7dpsf3jcd.us-east-1.elasticbeanstalk.com';
   }
 
   getInfo(placaCarro){
