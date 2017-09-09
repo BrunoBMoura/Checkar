a49b8032897fb376ab2389f53a13ea777e7c73a8
Victor Lúcio <victor.lucio10@gmail.com>
2017-09-08 22:49:10 -0700

    Add files via upload

--- scripts Python/program.py
+++ scripts Python/program.py
@@ -17,41 +17,41 @@ def info_cnw(marca, anoModelo):
 	#url = "http://www.carrosnaweb.com.br/avancada.asp"
 	url = "http://www.carrosnaweb.com.br/catalogo.asp?anoini=" + ano + "&anofim=" + ano + "&valfim=100000000&config=0&combustivel=0&cambio=0&proced=0&fabricante=" + fabricante + "&porte=0&varnome=" + modelo + "&portas=&ordem=1&passageiros=0"
 	#url = "http://www.carrosnaweb.com.br/catalogo.asp?anoini=1990&anofim=1990&valfim=100000000&config=0&combustivel=0&cambio=0&proced=0&fabricante=FORD&porte=0&varnome=ESCORT&portas=&ordem=1&passageiros=0"
-
+	print(url)
 	try:
 		html = requests.get(url)
 		text = html.text
 		soup = BeautifulSoup(text, "html5lib")
 		#x = soup.select("table tr td table tr td a")
 		x = soup.findAll(href=re.compile("fichadetalhe"))
-		#x = soup.findAll('a', href=True)
-		y = x[1::2]
-		title = []
-		href = []
-		for a in y:
-			title.append(a['title'])
-			href.append(a['href'])
-		closeMatch = difflib.get_close_matches(carro,title, 1, 0.1)[0]
-		endereco = "http://www.carrosnaweb.com.br/" + href[title.index(closeMatch)]
-		print("Modelo: " + closeMatch)
-		print("Link: " + endereco)
+		if len(x) > 0:
+			y = x[1::2]
+			title = []
+			href = []
+			for a in y:
+				title.append(a['title'].replace(u'\xa0', u' '))
+				href.append(a['href'])
+			closeMatch = difflib.get_close_matches(carro,title, 1, 0.1)[0]
+			endereco = "http://www.carrosnaweb.com.br/" + href[title.index(closeMatch)]
+			print("Modelo: " + closeMatch)
+			print("Link: " + endereco)
 
-		try:
-			html = requests.get(endereco)
-			text = html.text
-			soup = BeautifulSoup(text, "html5lib")
-			x = soup.findAll(href=re.compile(modelo.lower()))
-			for a in x:
-				print("http://www.carrosnaweb.com.br/" + a['href'])
-
-		except urllib.error.HTTPError:
-			pass
-
+			try:
+				html = requests.get(endereco)
+				text = html.text
+				soup = BeautifulSoup(text, "html5lib")
+				x = soup.findAll(href=re.compile(modelo.lower()))
+				for a in x:
+					print("http://www.carrosnaweb.com.br/" + a['href'])
+				print(soup.find('td', string = 'Ano').parent.findAll('a')[1].text) #preço
+			except urllib.error.HTTPError:
+				pass
+		else:
+			info_cnw(marca, str(int(anoModelo)-1))
 	except urllib.error.HTTPError:
 		pass
-
 #marca = "FORD/ESCORT 1.8 XR3"
 #anoModelo = "1990"
-marca = "PEUGEOT/HOGGAR ESCAPADE"
-anoModelo = "2012"
+marca = "Volkswagen/spacefox trend"
+anoModelo = "2014"
 info_cnw(marca, anoModelo)
