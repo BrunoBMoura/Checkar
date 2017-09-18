97fb73dc7dded7e7f67ac83727df10f9c164c9fd
lerafozo <rafael.leper@hotmail.com>
2017-09-18 18:46:36 -0300

    Web Motors 1.0
    Funfando, vamo começar os testes para perceber algum erro.
    Pra funcionar tem que dar instalar umas paradas:
    npm -g install phantomjs-prebuilt

--- scripts Python/webmotors/precoWebMotors.py
+++ scripts Python/webmotors/precoWebMotors.py
@@ -5,8 +5,8 @@ import difflib
 from os import *
 from bs4 import BeautifulSoup	
 from unidecode import *
-
-
+import re
+from selenium import webdriver #mandar um <npm -g install phantomjs-prebuilt> para funcionar
 #funcao para pegar informacoes de um carro no site carrosnaweb.com.br
 def info_webMotors(marca, anoModelo):
 	separador = marca.find("/")
@@ -19,27 +19,30 @@ def info_webMotors(marca, anoModelo):
 	ano = anoModelo
 
 	url = "https://www.webmotors.com.br/carros/estoque/"+fabricante+"/"+modelo+"/de."+ano+"/ate."+ano
-	print(url)
+	#print(url)
 	try:
 		html = requests.get(url)
 		text = html.text
 		soup = BeautifulSoup(text, "html5lib")
-		#x = soup.select("table tr td table tr td a")
 		x = soup.find('a', {'data-link':'click-abrir-anuncio'})
-		print(x['href'])
+		#print(x['href'])
 		if len(x)>0:
 			try:
-				html = requests.get(x['href'])
-				txt = html.text
-				soup = BeautifulSoup(text,"html5lib")
-				y = soup.find("div")
-				print y
+				driver = webdriver.PhantomJS()
+				driver.get(x['href'])
+				html = driver.page_source
+				driver.close()
+				soup = BeautifulSoup(html,"html5lib")
+				y = soup.find("div",{'class':'dis-tc col-5 last size-default'})
+				separador1=y.text.find("Médio") #acha a palavra medio
+				separador1+=10 #pula até o numero
+				separador2=y.text.find(",",separador1) #corta a casa decimal
+				precoMedio=y.text[separador1:separador2]
+				return precoMedio
 			except urllib.error.HTTPError:
 				pass
 	except urllib.error.HTTPError:
 		pass
-#marca = "FORD/ESCORT 1.8 XR3"
-#anoModelo = "1990"
-marca = "Volkswagen/spacefox trend"
+marca = "bmw/m5"
 anoModelo = "2014"
-info_webMotors(marca, anoModelo)
+print ("Preco médio é R$ "+ info_webMotors(marca, anoModelo)+",00")
