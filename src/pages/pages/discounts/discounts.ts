98565d5d7598763c96c6a0a1c9d376ee575477d4
ThiagoAkinori <thiago.akinori@gmail.com>
2017-09-22 16:31:34 -0300

    Add files via upload

---
+++ src/pages/pages/discounts/discounts.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'discounts',
+  templateUrl: 'discounts.html'
+})
+
+export class DiscountsPage {
+     
+}
